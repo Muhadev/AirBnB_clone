d9eb5ed033644b0fc8b66747bcf0fd75f6d20efd
Muhadev <muhammedfayemi@gmail.com>
2024-01-12 22:59:23 +0000

    task is added

--- console.py
+++ console.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import re
+from shlex import split
 import cmd
 from models import storage
 from models.base_model import BaseModel
@@ -165,3 +167,20 @@ class HBNBCommand(cmd.Cmd):
 if __name__ == '__main__':
     """Instantiate HBNBCommand and start the command loop"""
     HBNBCommand().cmdloop()
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
