fcb44aaf87c2ad09b266127ef7ee3cc19e7129fb
Muhadev <muhammedfayemi@gmail.com>
2024-01-14 02:49:07 +0000

    updated test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -14,61 +14,35 @@ from models.review import Review
 from models.engine.file_storage import FileStorage
 
 
-class TestStateDocs(unittest.TestCase):
+class TestFileStorageDocs(unittest.TestCase):
     """ check for documentation """
     def test_class_doc(self):
         """ check for class documentation """
-        self.assertTrue(len(State.__doc__) > 0)
+        self.assertTrue(len(FileStorage.__doc__) > 0)
 
 
-class TestStatePep8(unittest.TestCase):
+class TestFileStoragePep8(unittest.TestCase):
     """ check for pep8 validation """
     def test_pep8(self):
         """ test base and test_base for pep8 conformance """
         style = pep8.StyleGuide(quiet=True)
-        file1 = 'models/state.py'
-        file2 = 'tests/test_models/test_state.py'
+        file1 = 'models/engine/file_storage.py'
+        file2 = 'tests/test_models/test_engine/test_file_storage.py'
         result = style.check_files([file1, file2])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warning).")
 
 
-class TestState(unittest.TestCase):
-    """ tests for class State """
+class TestFileStorage(unittest.TestCase):
+    """ tests for class FileStorage """
     @classmethod
     def setUpClass(cls):
         """ set up instances for all tests """
-        cls.state = State()
+        storage = FileStorage()
 
-    def test_subclass(self):
-        """ test that state is a subclass of basemodel """
-        self.assertIsInstance(self.state, BaseModel)
-        self.assertTrue(hasattr(self.state, "id"))
-        self.assertTrue(hasattr(self.state, "created_at"))
-        self.assertTrue(hasattr(self.state, "updated_at"))
-
-    def test_id(self):
-        """ test id """
-        self.assertEqual(str, type(self.state.id))
-
-    def test_created_at(self):
-        """ test created_at """
-        self.assertEqual(datetime, type(self.state.created_at))
-
-    def test_updated_at(self):
-        """ test updated_at """
-        self.assertEqual(datetime, type(self.state.updated_at))
-
-    def test_name(self):
-        """ test name """
-        self.assertTrue(hasattr(self.state, "name"))
-        self.assertEqual(self.state.name, "")
-
-    def test_to_dict(self):
-        """ test to_dict method """
-        new_dict = self.state.to_dict()
-        self.assertEqual(type(new_dict), dict)
-        self.assertTrue('to_dict' in dir(self.state))
+    def test_all(self):
+        """ test all method """
+        pass
 
     @classmethod
     def tearDownClass(cls):
