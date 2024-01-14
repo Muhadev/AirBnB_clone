1a2989550923262a68c3e73d2b1330fcc9c55eb6
Muhadev <muhammedfayemi@gmail.com>
2024-01-14 06:21:09 -0800

    task is fix

--- models/base_model.py
+++ models/base_model.py
@@ -38,7 +38,6 @@ class BaseModel:
         """String representation for instances"""
         return f"[{type(self).__name__}] ({self.id}) <{self.__dict__}>"
 
-
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of
         the instance"""
