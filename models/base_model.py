2452ee6687537e67ebfa8b444987b83c215ea32f
Muhadev <muhammedfayemi@gmail.com>
2024-01-14 06:33:55 -0800

    task is fix

--- models/base_model.py
+++ models/base_model.py
@@ -38,6 +38,11 @@ class BaseModel:
         """String representation for instances"""
         return f"[{type(self).__name__}] ({self.id}) <{self.__dict__}>"
 
+    def save(self):
+        """updates the public instance attribute `updated_at` with
+        the current datetime"""
+        self.updated_at = datetime.now()
+
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of
         the instance"""
